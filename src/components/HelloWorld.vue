f26b43e16ea5b958b7db915099ed63146c89cfa6
zhoujy <1055072412@qq.com>
2022-09-21 18:51:33 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <video controls id="videoEle" width="500" height="300" muted>
+        </video>
+    </div>
+
+</template>
+
+<script>
+import flvjs from "flv.js"
+import videoUrl from "../assets/video2.mp4"
+export default {
+    name: "HelloWorld",
+    data() {
+        return {
+            src: require("../../static/vvas.flv"),
+            flvPlayer: '',
+        };
+    },
+    created() {
+        
+    },
+    mounted() {
+        if (flvjs.isSupported()) { 
+            var timer = setTimeout(() => {
+                var video = document.getElementById("videoEle");
+                this.flvPlayer = flvjs.createPlayer({
+                    type: "flv",
+                    url: this.src,
+                });
+                console.log(this.flvPlayer);
+                console.log(video)
+                this.flvPlayer.attachMediaElement(video);
+                this.flvPlayer.load();
+                this.flvPlayer.play();
+            }, 2000)
+        }else {
+            console.log('浏览器不支持flv')
+        }
+        
+
+        // this.$nextTick(() => {
+        //     var video = document.getElementById("video");
+        //     this.flvPlayer.attachMediaElement(video);
+        //     this.flvPlayer.load();
+        // });
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+    font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+</style>
---
+++ src/components/flvModule.vue
@@ -0,0 +1,40 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: liuyq
+ * @Date: 2022-07-04 18:26:16
+ * @LastEditors: zhoujy
+ * @LastEditTime: 2022-07-04 18:42:43
+-->
+<template>
+    <video ref="video" autoplay>
+        Your browser is too old which doesn't support HTML5 video.
+    </video>
+</template>
+<script>
+import flvjs from "flv.js"
+export default {
+    name: "flvModule",
+    data() {
+        return {
+            src:"https://sample-videos.com/video123/flv/720/big_buck_bunny_720p_2mb.flv",
+            flvPlayer:null,
+        }
+    },
+    created() {
+        this.$nextTick(() => {
+            if (flvjs.isSupported()) {
+                const flvPlayer = flvjs.createPlayer({
+                    type: "flv",
+                    isLive: true,
+                    url: this.src,
+                });
+                flvPlayer.attachMediaElement(this.$refs.video);
+                flvPlayer.load();
+                flvPlayer.play();
+                this.flvPlayer = flvPlayer;
+            }
+        });
+    },
+};
+</script>
